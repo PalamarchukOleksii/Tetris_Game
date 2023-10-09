65abda1fee1ea94fdc9af3998f7a6bc681584942
oleksiypalamarchuck@gmail.com <oleksiypalamarchuck@gmail.com>
2023-10-09 16:33:40 +0300

    Cmake

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: D:/VS code projects/Tetris_Game
+# Build directory: D:/VS code projects/Tetris_Game/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
